b1ed7306c5decd33b52431463ade3c693336f5a1
Harsh <harshwardhandatascientist@gmail.com>
2025-03-11 01:24:59 +0530

    Update main.tf
    m

--- prometheus-terraform/modules/networking/main.tf
+++ prometheus-terraform/modules/networking/main.tf
@@ -113,3 +113,8 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
+output "alb_dns_name" {
+  description = "The DNS name of the ALB"
+  value       = aws_lb.app_lb.dns_name
+}
+
