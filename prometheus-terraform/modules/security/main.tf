b33d202a94d0688852cfc145f77f5c29d799c0b1
Harsh <harshwardhandatascientist@gmail.com>
2025-03-11 01:39:50 +0530

    Update main.tf
    m

--- prometheus-terraform/modules/security/main.tf
+++ prometheus-terraform/modules/security/main.tf
@@ -69,6 +69,28 @@ resource "aws_security_group" "private_sg" {
     Name = "private-sg"
   }
 }
+resource "aws_security_group" "alb_sg" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
 
 output "public_sg_id" {
   value = aws_security_group.public_sg.id
