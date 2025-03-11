a47287241bb46cb97fd24c9fb2f0986e6452aa14
harsh <harshwardhandatascientist@gmail.com>
2025-03-11 14:09:46 +0530

    m

--- prometheus-terraform/outputs.tf
+++ prometheus-terraform/outputs.tf
@@ -5,3 +5,10 @@ output "alb_dns_name" {
   description = "The DNS name of the ALB"
   value       = module.networking.alb_dns_name
 }
+output "public_subnet_ids" {
+  value = module.networking.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.networking.private_subnet_ids
+}
