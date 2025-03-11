2f96beb05266ccca3e52000d3e8467068db26fb9
Harsh <harshwardhandatascientist@gmail.com>
2025-03-11 15:21:54 +0530

    Update backend.tf
    m

--- prometheus-terraform/backend.tf
+++ prometheus-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-websitej"  
+    bucket = "promtheus-harsh-bucket"  
     key    = "prometheus/terraform.tfstate" 
     region = "eu-north-1"                    
     encrypt = true                        
