cefa545e2d52d413a358f50eaace836e55f56e46
Harsh <harshwardhandatascientist@gmail.com>
2025-03-11 18:32:53 +0530

    Delete prometheus-terraform directory
    f

--- prometheus-terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "promtheus-harsh-bucket"  
-    key    = "prometheus/terraform.tfstate" 
-    region = "eu-north-1"                    
-    encrypt = true                        
-  }
-}
