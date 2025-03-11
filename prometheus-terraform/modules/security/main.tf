0a91e627a030f3c7391bb37faa23614b002c3206
harsh <harshwardhandatascientist@gmail.com>
2025-03-11 11:22:14 +0530

    k

--- prometheus-terraform/modules/security/main.tf
+++ prometheus-terraform/modules/security/main.tf
@@ -92,10 +92,4 @@ resource "aws_security_group" "alb_sg" {
 }
 
 
-output "public_sg_id" {
-  value = aws_security_group.public_sg.id
-}
 
-output "private_sg_id" {
-  value = aws_security_group.private_sg.id
-}
