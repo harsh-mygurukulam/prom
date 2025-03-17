daad81f67d439cd46d935f6a0e5aa1cedea0e715
Harsh <harshwardhandatascientist@gmail.com>
2025-03-18 00:36:59 +0530

    Update variables.tf
    M

--- prometheus-terraform/modules/instances/variables.tf
+++ prometheus-terraform/modules/instances/variables.tf
@@ -1,5 +1,8 @@
 variable "ami_id" {}
 variable "instance_type" {}
 variable "key_name" {}
-variable "public_subnet_ids" {}  # ✅ Update to be a list
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
 variable "public_sg_id" {}  # ✅ Only need public SG
