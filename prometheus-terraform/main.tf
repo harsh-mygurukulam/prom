f3ef230b4d9e24636c933acddc6a4d6c561a2eba
Harsh <harshwardhandatascientist@gmail.com>
2025-03-18 00:42:46 +0530

    Update main.tf
     N

--- prometheus-terraform/main.tf
+++ prometheus-terraform/main.tf
@@ -21,9 +21,7 @@ module "instances" {
   public_subnet_ids = module.networking.public_subnet_ids  # ✅ Pass list of subnets
   public_sg_id = module.security.public_sg_id
 }
-output "public_instance_ips" {
-  value = module.instances.public_instance_ips
-}
+
 
 
 
