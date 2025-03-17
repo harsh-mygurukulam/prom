5f1e326de93bd813b3f62ba3ec42440642dd0ff5
Harsh <harshwardhandatascientist@gmail.com>
2025-03-18 00:40:02 +0530

    Update outputs.tf
    M

--- prometheus-terraform/outputs.tf
+++ prometheus-terraform/outputs.tf
@@ -2,6 +2,6 @@ output "vpc_id" {
   value = module.networking.vpc_id
 }
 
-output "public_instance_ip" {
-  value = module.instances.public_instance_ip
+output "public_instance_ips" {
+  value = module.instances.public_instance_ips
 }
