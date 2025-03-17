9b1432fd2272aba0140ee99eb080c6b3e23a6400
Harsh <harshwardhandatascientist@gmail.com>
2025-03-17 22:16:22 +0530

    Update main.tf
    comit

--- prometheus-terraform/main.tf
+++ prometheus-terraform/main.tf
@@ -6,7 +6,6 @@ module "networking" {
   source = "./modules/networking"
   vpc_cidr = "192.168.0.0/16"
   public_subnets  = ["192.168.10.0/24", "192.168.20.0/24"]
-  private_subnets = ["192.168.30.0/24", "192.168.40.0/24"]
 }
 
 module "security" {
@@ -20,15 +19,9 @@ module "instances" {
   instance_type = "t3.micro"
   key_name = "ansible"
   public_subnet_id  = module.networking.public_subnet_ids[0]
-  private_subnet_id = module.networking.private_subnet_ids[0]
   public_sg_id  = module.security.public_sg_id
-  private_sg_id = module.security.private_sg_id
 }
 
 output "public_instance_ip" {
   value = module.instances.public_instance_ip
-}
-
-output "private_instance_ip" {
-  value = module.instances.private_instance_ip
 }
