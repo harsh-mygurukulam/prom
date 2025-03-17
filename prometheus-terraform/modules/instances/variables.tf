446fbc4ced87b66092c3fcca1acbbcbf83cdfcfd
Harsh <harshwardhandatascientist@gmail.com>
2025-03-18 00:05:42 +0530

    Update variables.tf
    commited

--- prometheus-terraform/modules/instances/variables.tf
+++ prometheus-terraform/modules/instances/variables.tf
@@ -1,9 +1,5 @@
 variable "ami_id" {}
 variable "instance_type" {}
 variable "key_name" {}
-
-variable "public_subnet_ids" {  # ✅ Change `public_subnet_id` → `public_subnet_ids` (list of subnets)
-  type = list(string)
-}
-
-variable "public_sg_id" {}  # ✅ Keep public security group, remove private one
+variable "public_subnet_ids" {}  # ✅ Update to be a list
+variable "public_sg_id" {}  # ✅ Only need public SG
