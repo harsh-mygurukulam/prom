4f73dac1aed05a3cb484bae2453ea83c7bb4cc03
Harsh <harshwardhandatascientist@gmail.com>
2025-03-21 22:26:01 +0530

    Update variables.tf
    m

--- prometheus-terraform/modules/networking/variables.tf
+++ prometheus-terraform/modules/networking/variables.tf
@@ -11,5 +11,5 @@ variable "vpc_cidr" {}
 
 variable "public_subnets" {
   type = list(string)
-  default = ["192.168.10.0/24", "192.168.20.0/24"]  # ✅ Sirf 2 public subnets
+  default = ["192.168.10.0/24", "192.168.20.0/24"]  
 }
